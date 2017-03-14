3b8b5fb67c390069d806ef74481f6c4e5b7a60e5
SudoKel <kjoanes@gmail.com>
2017-03-14 18:33:29 -0230

    Created motion data view class

---
+++ P2PMotion/MotionDataView.h
@@ -0,0 +1,13 @@
+//
+//  MotionDataView.h
+//  P2PMotion
+//
+//  Created by Kelwin Socorro Savio Joanes on 2017-03-14.
+//  Copyright © 2017 Kelwin Joanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MotionDataView : UIView
+
+@end
---
+++ P2PMotion/MotionDataView.m
@@ -0,0 +1,20 @@
+//
+//  MotionDataView.m
+//  P2PMotion
+//
+//  Created by Kelwin Socorro Savio Joanes on 2017-03-14.
+//  Copyright © 2017 Kelwin Joanes. All rights reserved.
+//
+
+#import "MotionDataView.h"
+
+@implementation MotionDataView
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+    
+}
+
+@end
